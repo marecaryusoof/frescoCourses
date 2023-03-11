ec3fb9e679264b6f1ad12006be18a540d186d74a
marecaryusoof <41865498+marecaryusoof@users.noreply.github.com>
2023-03-11 16:05:27 +0530

    Added Files

---
+++ src/main/java/springBoot/frescoCourses/FrescoCoursesApplication.java
@@ -0,0 +1,13 @@
+package springBoot.frescoCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrescoCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrescoCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springBoot/frescoCourses/FrescoCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package springBoot.frescoCourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrescoCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
